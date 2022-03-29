5876472e8cf17fff93f06a6fc8a3a722b5fb456c
Julien Marquet <j@recursor.wf>
2022-03-29 16:46:11 +0200

    update copyright year

--- Flows/Notation.lean
+++ Flows/Notation.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2021 Julien Marquet. All rights reserved.
+Copyright (c) 2021-2022 Julien Marquet. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Julien Marquet
 -/
